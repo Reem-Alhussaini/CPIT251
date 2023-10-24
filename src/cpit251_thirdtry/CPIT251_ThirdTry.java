0a5f8d866c4aeb5fd9251dad2dee99c0d17efa07
Reem-Alhussaini <ReemAlhussaini81@outlook.com>
2023-10-24 04:21:19 +0300

    first commit

---
+++ src/cpit251_thirdtry/CPIT251_ThirdTry.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cpit251_thirdtry;
+
+/**
+ *
+ * @author ryo0m
+ */
+public class CPIT251_ThirdTry {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
